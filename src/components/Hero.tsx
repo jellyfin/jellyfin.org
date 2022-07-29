adb333c74f5388af5ad8a3a3da582092b089ef59
Bill Thornton <billt2006@gmail.com>
2022-07-29 11:35:36 -0400

    Move hero to component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx';
+import React, { FunctionComponent, ReactNode } from 'react';
+
+import './Hero.css';
+
+type HeroProps = {
+  children?: ReactNode,
+  title: string,
+  large?: boolean
+}
+
+const Hero: FunctionComponent<HeroProps> = ({
+  children,
+  title,
+  large = false
+}: HeroProps) => (
+  <header className={clsx('hero', { 'hero--large': large })}>
+    <div className='hero__overlay hero__overlay--gradient'></div>
+    <div className='hero__inner'>
+      <div className='container'>
+        <div className='hero__content'>
+          <div className='hero__content__inner'>
+            <h1 className='hero__title'>{title}</h1>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+);
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,11 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '../components/HomepageFeatures';
 import FreeSoftware from '../components/FreeSoftware';
 import BuiltByVolunteers from '../components/BuiltByVolunteers';
 import MoreClients from '../components/MoreClients';
 import CallToAction from '../components/CallToAction';
-
-function HomepageHeader() {
-  return (
-    <header className={clsx('hero')}>
-      <div className="hero-overlay hero-overlay--gradient"></div>
-      <div className="hero-inner">
-        <div className="container">
-          <div className="hero-content">
-            <div className="hero-content-inner">
-              <h1 className="hero-title">The Free Software Media System</h1>
-              <p className="hero-text">
-                Jellyfin is the volunteer-built media solution that puts{' '}
-                <em>you</em> in control of your media. Stream to any device from
-                your own server, with no strings attached. Your media, your
-                server, your way.
-              </p>
-              <a
-                href="https://demo.jellyfin.org/stable"
-                className="button button--secondary button--outline"
-              >
-                See it in Action
-              </a>
-              <a
-                href="/downloads"
-                className="button button button--primary margin-horiz--md"
-              >
-                Download Now
-              </a>
-              <button className="button button--secondary button--outline scroll">
-                Learn More
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-}
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
@@ -51,7 +13,29 @@ export default function Home() {
       title={`The Free Software Media System`}
       description="The volunteer-built media solution that puts you in control of your media. Stream to any device from your own server, with no strings attached."
     >
-      <HomepageHeader />
+      <Hero title="The Free Software Media System" large>
+        <p className="hero__text">
+          Jellyfin is the volunteer-built media solution that puts{' '}
+          <em>you</em> in control of your media. Stream to any device from
+          your own server, with no strings attached. Your media, your
+          server, your way.
+        </p>
+        <a
+          href="https://demo.jellyfin.org/stable"
+          className="button button--secondary button--outline"
+        >
+          See it in Action
+        </a>
+        <a
+          href="/downloads"
+          className="button button button--primary margin-horiz--md"
+        >
+          Download Now
+        </a>
+        <button className="button button--secondary button--outline scroll">
+          Learn More
+        </button>
+      </Hero>
       <main>
         <HomepageFeatures />
         <FreeSoftware />
