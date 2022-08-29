c1c07c6802a380b8fb01f864db88de508e7bb0a0
Bill Thornton <thornbill@users.noreply.github.com>
2022-08-29 16:03:51 -0400

    Fix page component names

--- src/pages/downloads/macos.tsx
+++ src/pages/downloads/macos.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import DownloadsPage from '.';
 import { OsType } from '../../data/downloads';
 
-export default function LinuxDownloads() {
+export default function MacOSDownloads() {
   return <DownloadsPage osType={OsType.MacOS} />;
 }
--- src/pages/downloads/windows.tsx
+++ src/pages/downloads/windows.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import DownloadsPage from '.';
 import { OsType } from '../../data/downloads';
 
-export default function LinuxDownloads() {
+export default function WindowsDownloads() {
   return <DownloadsPage osType={OsType.Windows} />;
 }
