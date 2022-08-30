49d9ba9acca343af8f9849da3ae8ce923d3e4077
Bill Thornton <thornbill@users.noreply.github.com>
2022-08-30 12:18:59 -0400

    Cleanup styling of home page hero and cta sections

--- src/components/home/CallToAction.tsx
+++ src/components/home/CallToAction.tsx
@@ -5,13 +5,13 @@ export default function CallToAction() {
   return (
     <div className={styles.cta}>
       <div className='container'>
-        <div className='cta-inner'>
-          <h2 className='cta-title'>Get Started Now</h2>
-          <p className='cta-sub'>Check out our Getting Started guide to download and set up your server today.</p>
-          <a href='/downloads' className='button button--primary button--lg margin-top--lg'>
-            Download Jellyfin
-          </a>
-        </div>
+        <h2>Get Started Now</h2>
+        <p className={styles['cta-sub']}>
+          Check out our Getting Started guide to download and set up your server today.
+        </p>
+        <a href='/downloads' className='button button--primary button--lg margin-top--lg'>
+          Download Jellyfin
+        </a>
       </div>
     </div>
   );
