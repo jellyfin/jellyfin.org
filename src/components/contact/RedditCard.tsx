7c8674af2a8979a8cef675cf7c6a524f5c225e9f
Anthony Lavado <anthonylavado@me.com>
2023-06-11 13:37:09 -0400

    Update RedditCard.tsx

--- src/components/contact/RedditCard.tsx
+++ src/components/contact/RedditCard.tsx
@@ -12,13 +12,16 @@ const RedditCard = () => (
       <Reddit />
     </div>
     <div className='card__body'>
-      r/Jellyfin has <a href='https://www.theverge.com/2023/6/5/23749188/reddit-subreddit-private-protest-api-changes-apollo-charges'>gone private</a>. Our chat options remain open.
-      We are currently on Lemmy: <a href="https://beehaw.org/c/jellyfin@lemmy.ml">c/Jellyfin</a> (via Beehaw)
+      r/Jellyfin has
+      <a href='https://www.theverge.com/2023/6/5/23749188/reddit-subreddit-private-protest-api-changes-apollo-charges'>
+        gone private
+      </a>
+      . Our chat options remain open. We are currently on Lemmy:
+      <a href="https://beehaw.org/c/jellyfin@lemmy.ml">c/Jellyfin</a> (via Beehaw)
     </div>
     <div className='card__footer'>
       <button className='button button--block button--reddit'>
-Unavailable
-      </button>
+Unavailable</button>
     </div>
   </div>
 );
