5eb14db47129bdbc666599530873c931910433ca
Peaches_MLG <PeachesMLG100@gmail.com>
2024-03-07 21:48:52 +0000

    Added a work in progress banner under the /developers page

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Banner.scss';
+
+function Banner({ text }: { text: string }) {
+  return (
+    <div className="banner">
+      {text}
+    </div>
+  );
+}
+
+export default Banner;
+
