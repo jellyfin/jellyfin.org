a9e73a8a7e2bbfcbab9eca2e9dcad42f419ef81b
Niels van Velzen <git@ndat.nl>
2024-03-11 19:16:09 +0100

    Reformat + update text

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 import './Banner.scss';
 
 function Banner({ text }: { text: string }) {
-  return (
-    <div className="banner">
-      {text}
-    </div>
-  );
+  return <div className='banner'>{text}</div>;
 }
 
 export default Banner;
-
