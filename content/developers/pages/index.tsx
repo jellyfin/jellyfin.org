7e80b3bfcee61324d96bb12892cb0af947921c2a
Peaches_MLG <PeachesMLG100@gmail.com>
2024-03-11 18:30:09 +0000

    Refactor code to reduce duplication

--- content/developers/pages/index.tsx
+++ content/developers/pages/index.tsx
@@ -1,86 +1,41 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import Hero from '../../../src/components/common/Hero';
-import blogPosts from '../../../.docusaurus/docusaurus-plugin-content-blog/developers-blog/blog-post-list-prop-developers-blog.json';
+import blogPosts
+  from '../../../.docusaurus/docusaurus-plugin-content-blog/developers-blog/blog-post-list-prop-developers-blog.json';
 
 import './index.scss';
+import NavigationLinks from '../../../src/components/navigationLinks/NavigationLinks';
 
-function Section({ title, children }: { title: string; children: ReactNode }) {
-  return (
-    <div className='col col--4 margin-top--md margin-bottom--md'>
-      <div className='card card--dev-index'>
-        <h2>{title}</h2>
-        {children}
-      </div>
-    </div>
-  );
-}
-
-function DocsHighlights() {
-  return (
-    <Section title='Documentation'>
-      <ul>
-        <li>
-          <Link to='/developers/docs/api/authorization'>Using the REST API</Link>
-        </li>
-        <li>
-          <Link to='/developers/docs/api/authorization'>Creating a server plugin</Link>
-        </li>
-      </ul>
-    </Section>
-  );
-}
-
-function OtherPages() {
-  return (
-    <Section title='More resources'>
-      <ul>
-        <li>
-          <Link to='/developers/docs/contributing/'>Contributing</Link>
-        </li>
-        <li>
-          <Link to='/developers/docs/branding'>Jellyfin branding</Link>
-        </li>
-      </ul>
-    </Section>
-  );
-}
-
-function RecentBlogPosts() {
-  return (
-    <Section title={blogPosts.title}>
-      <ul>
-        {blogPosts.items.slice(0, 5).map((item, index) => (
-          <li key={index}>
-            <a href={`${item.permalink}`}>{item.title}</a>{' '}
-          </li>
-        ))}
-      </ul>
-    </Section>
-  );
-}
-
-function Index() {
+export default function Index() {
   return (
     <Layout title={`Jellyfin developers`}>
-      <Hero title='Jellyfin for Developers'>
-        <p className='hero__text margin-vert--lg'>Get started developing with or for Jellyfin today.</p>
-        <div className='hero__buttons'>
-          <Link to='/docs/'>Not a developer? Go to the user documentation</Link>
+      <Hero title="Jellyfin for Developers">
+        <p className="hero__text margin-vert--lg">Get started developing with or for Jellyfin today.</p>
+        <div className="hero__buttons">
+          <Link to="/docs/">Not a developer? Go to the user documentation</Link>
         </div>
       </Hero>
       <main>
-        <section className='container'>
-          <div className='row'>
-            <DocsHighlights />
-            <RecentBlogPosts />
-            <OtherPages />
+        <section className="container">
+          <div className="row">
+            <NavigationLinks title="Documentation" pages={[
+              { url: '/developers/docs/api/authorization', name: 'Using the REST API' },
+              { url: '/developers/docs/api/authorization', name: 'Creating a server plugin' }
+            ]} />
+
+            <NavigationLinks title={blogPosts.title} pages={blogPosts.items.slice(0, 5).map((item) => {
+              return { url: item.permalink, name: item.title };
+            })} />
+
+            <NavigationLinks title="More resources" pages={[
+              { url: '/developers/docs/contributing', name: 'Contributing' },
+              { url: '/developers/docs/branding', name: 'Jellyfin branding' }
+            ]} />
           </div>
         </section>
       </main>
     </Layout>
   );
 }
-
-export default Index;
