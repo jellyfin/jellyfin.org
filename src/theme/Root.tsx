45eb650dc2b34817c9222c46bdd0964828a34ba8
Niels van Velzen <git@ndat.nl>
2024-03-11 19:16:09 +0100

    Reformat + update text

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { useLocation } from '@docusaurus/router';
 import Banner from '../components/banner/Banner';
 
-// The root view wrapper for the entire app
+/**
+ * The root view wrapper for the entire app.
+ */
 export default function Root({ children }: { children: React.ReactNode }) {
   const isInDevelopersSection = useLocation().pathname.startsWith('/developers');
 
-  return <>
-    {isInDevelopersSection && <Banner text="This page is still a work in progress. Some content may be incomplete." />}
-    {children}
-  </>;
+  return (
+    <>
+      {isInDevelopersSection && (
+        <Banner text='The developer site is still in development. Some content may be incomplete or missing.' />
+      )}
+      {children}
+    </>
+  );
 }
