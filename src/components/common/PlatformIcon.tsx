2b20257a69faac91a79581eec3a29e94654b012e
BotBlake <B0TBlake@protonmail.com>
2025-08-03 23:50:00 +0200

    add generic Xbox Icon

--- src/components/common/PlatformIcon.tsx
+++ src/components/common/PlatformIcon.tsx
@@ -117,6 +117,9 @@ const PlatformIcon = ({
           <path d='M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z' />
         </svg>
       );
+    case Platform.Xbox:
+      // Not available in simple-icons because it was removed due to Microsoft (https://github.com
+      return <Icon path={mdiMonitor} size={`${size}px`} className={className} />;
 
     default:
       return null;
