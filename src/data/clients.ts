d735eff9891c569c7a977cc16b6e3a8e25d56871
kp <94090994+kpgamingz@users.noreply.github.com>
2025-09-23 18:52:15 -0400

    Remove Jellyfin for Xbox client entry
    Removed Jellyfin for Xbox client details from the clients list.

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -1017,30 +1017,6 @@ const thirdPartyClients: Array<Client> = [
         url: 'https://livefin.github.io/web/'
       }
     ]
-  },
-  {
-    id: 'jellyfin-xbox',
-    name: 'Jellyfin for Xbox',
-    description: 'The official Jellyfin app for Xbox consoles.',
-    clientType: ClientType.Official,
-    deviceTypes: [DeviceType.TV],
-    licenseType: LicenseType.OpenSource,
-    platforms: [Platform.Xbox],
-    primaryLinks: [
-      {
-        id: 'microsoft-store',
-        name: 'Microsoft Store',
-        url: 'https://apps.microsoft.com/detail/9P2DRTG62QF8'
-      }
-    ],
-    secondaryLinks: [
-      {
-        id: 'github',
-        name: 'GitHub',
-        url: 'https://github.com/jellyfin/jellyfin-xbox'
-
-      }
-    ]
   }
 ];
 
