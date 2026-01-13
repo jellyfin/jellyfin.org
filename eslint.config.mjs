9e7f23c108f7b3b6cc5ce38222ed0d25c505ca7c
BotBlake <B0TBlake@protonmail.com>
2026-01-13 10:49:50 +0100

    remove unused var

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { globalIgnores } from 'eslint/config';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import ts from 'typescript-eslint';
