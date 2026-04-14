9a9f51253f181c2e6b46ee22985fcc741ccac43d
BotBlake <B0TBlake@protonmail.com>
2026-04-14 22:10:12 +0200

    remove irc card

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,15 +2,12 @@ import Layout from '@theme/Layout';
 import React, { useState } from 'react';
 
 import DiscordCard from '../components/contact/DiscordCard';
-import IrcCard from '../components/contact/IrcCard';
 import MatrixCard from '../components/contact/MatrixCard';
 import ForumCard from '../components/contact/ForumCard';
 import TwitterCard from '../components/contact/TwitterCard';
 import MastodonCard from '../components/contact/MastodonCard';
 
 export default function Contact() {
-  const [isOtherChatVisible, setOtherChatVisible] = useState(false);
-
   return (
     <Layout title='Contact'>
       <h1 className='text--center margin-top--lg'>Contact</h1>
@@ -30,25 +27,6 @@ export default function Contact() {
               <DiscordCard />
             </div>
           </div>
-          <div className='row'>
-            <div className='col margin-bottom--md text--center'>
-              <button
-                className='button button--sm button--secondary button--outline'
-                onClick={() => {
-                  setOtherChatVisible(!isOtherChatVisible);
-                }}
-              >
-                Other
-              </button>
-            </div>
-          </div>
-          {isOtherChatVisible && (
-            <div className='row'>
-              <div className='col col--12 margin-bottom--md'>
-                <IrcCard />
-              </div>
-            </div>
-          )}
           <div className='row'>
             <div className='col'>
               <h2>Social</h2>
