35f1bfa0866aa216d81a26a36136fff0b8ac02c1
BotBlake <B0TBlake@protonmail.com>
2026-05-06 02:17:17 +0200

    swizzle blog sidebar

---
+++ src/theme/BlogSidebar/Content/index.tsx
@@ -0,0 +1,45 @@
+import React, { memo, type ReactNode } from 'react';
+import { useThemeConfig } from '@docusaurus/theme-common';
+import { groupBlogSidebarItemsByYear } from '@docusaurus/plugin-content-blog/client';
+import Heading from '@theme/Heading';
+import type { Props } from '@theme/BlogSidebar/Content';
+
+function BlogSidebarYearGroup({
+  year,
+  yearGroupHeadingClassName,
+  children
+}: {
+  year: string;
+  yearGroupHeadingClassName?: string;
+  children: ReactNode;
+}) {
+  return (
+    <div role='group'>
+      <Heading as='h3' className={yearGroupHeadingClassName}>
+        {year}
+      </Heading>
+      {children}
+    </div>
+  );
+}
+
+function BlogSidebarContent({ items, yearGroupHeadingClassName, ListComponent }: Props): ReactNode {
+  const themeConfig = useThemeConfig();
+  return (
+    <>
+      {themeConfig.blog.sidebar.groupByYear ? (
+        <>
+          {groupBlogSidebarItemsByYear(items).map(([year, yearItems]) => (
+            <BlogSidebarYearGroup key={year} year={year} yearGroupHeadingClassName={yearGroupHeadingClassName}>
+              <ListComponent items={yearItems} />
+            </BlogSidebarYearGroup>
+          ))}
+        </>
+      ) : (
+        <ListComponent items={items} />
+      )}
+    </>
+  );
+}
+
+export default memo(BlogSidebarContent);
